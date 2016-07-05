a6183ce0d561a3f58eeee68db1094226eeaad8dc
David B. Dahl <david@ddahl.org>
2016-07-05 09:12:56 -0600

    More recursion debugging.

--- benchmarks/recursion.R
+++ benchmarks/recursion.R
@@ -24,7 +24,29 @@ g <- function(counter) {
 }
 
 
+# Oops, the output is messed up.
+hh <- s$def('x: Int','
+  println(s"Hello $x from Scala.")
+  R.eval(s"h(${x+1})")
+')
+
+h <- function(counter) {
+  if ( counter >= 10 ) return()
+  cat("Hello",counter,"from R.\n")
+  hh(counter)
+}
+h(0)
+
+
+# Why doesn't this work?
+ii <- s$def('x: Int','
+  if ( x > 100 ) x
+  else x + R.evalI0(s"ii(${x+1})")
+')
+ii(0)
+
+
 library(microbenchmark)
 
-microbenchmark(f(0),g(0),times=5)
+microbenchmark(f(0),g(0),h(0),times=5)
 
