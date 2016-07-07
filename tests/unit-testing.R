db8928f63403cec36a4232dac3eb11068e26e08f
David B. Dahl <david@ddahl.org>
2016-07-06 22:29:19 -0600

    Added unit-testing script.

---
+++ tests/unit-testing.R
@@ -0,0 +1,65 @@
+test <- function(a,value,func=list(),method=c("name","$")[1]) {
+  if ( method == "name" ) {
+    intpSet(a,"tmp",value)
+    tmp <- intpGet(a,"tmp")
+  } else if ( method == "$" ) {
+    a$tmp <- value
+    tmp <- a$tmp
+  } else stop("Unrecognized method.")
+  if ( length(func) > 0 ) {
+    if ( ! is.list(func) ) func <- list(func)
+    for ( f in func ) {
+      if ( ! f(tmp) ) return(FALSE)
+    }
+  }
+  if ( class(tmp) != class(value) ) return(FALSE)
+  if ( length(tmp) != length(value) ) return(FALSE)
+  if ( any(tmp != value) ) return(FALSE)
+  TRUE
+}
+
+assertAll <- function(interpreter) {
+  a <- interpreter
+  assert <- function(x) {
+    if ( ! x ) stop("Assertion not true.")
+  }
+  for ( method in c("name","$") ) {
+    cat("Testing method:",method,"\n")
+    assert(test(a,"Lisa",method=method))
+    assert(test(a,c("Lisa","Susan","Grace","Evalyn"),method=method))
+    assert(test(a,matrix(c("a","b","c","d","e","f"),nrow=3),method=method))
+    assert(test(a,1,method=method))
+    assert(test(a,c(1,2,3,4),method=method))
+    assert(test(a,matrix(c(1,2,3,4,5,6),nrow=3),method=method))
+    assert(test(a,TRUE,method=method))
+    assert(test(a,as.integer(c(TRUE,FALSE)),method=method))
+    assert(test(a,matrix(c(TRUE,TRUE,FALSE,TRUE,FALSE,TRUE),nrow=3),method=method))
+    assert(test(a,1L,method=method))
+    assert(test(a,as.integer(c(1,2,3,4)),method=method))
+    assert(test(a,matrix(as.integer(c(1,2,3,4,5,6)),nrow=3),method=method))
+  }
+}
+
+library(rscala)
+
+# General tests
+serialize <- as.logical(Sys.getenv("RSCALA_SERIALIZE"))
+cat(serialize,"\n")
+a <- scalaInterpreter(serialize=serialize)
+
+cat(a %~% "util.Properties.versionNumberString","\n")
+assertAll(a)
+
+
+# Tests of static typing in Scala
+a$b <- matrix(1L:12L,ncol=3)
+a %~% "println(b(1)(2))"
+a$b <- matrix(c(FALSE,TRUE),ncol=2)
+a %~% "println(b(0)(1))"
+a$b <- seq(0,1,length=5)
+a %~% "println(b(3))"
+a$b <- FALSE
+a %~% "println(!b)"
+
+close(a)
+rm(a)
