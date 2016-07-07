4d431cd4c7ae775baec218edd136b7dbb6c5eff3
David B. Dahl <david@ddahl.org>
2016-07-06 22:41:49 -0600

    Added mistakes test script.

---
+++ tests/mistakes.R
@@ -0,0 +1,62 @@
+library(rscala)
+
+serialize <- as.logical(Sys.getenv("RSCALA_SERIALIZE"))
+cat(serialize,"\n")
+s <- scalaInterpreter(serialize=serialize)
+
+s %~% "util.Properties.versionNumberString"
+
+a <- list(a=3,b=4)
+tryCatch(s$j <- a,error=function(e) {'Caught'})
+s$.j <- a
+identical(a,intpUnwrap(s,s$.j))
+s$j
+s$jj <- unlist(a)
+
+tryCatch(s$j <- complex(3,4,5,32),error=function(e) {'Caught'})
+
+s %~% "null"
+
+s %~% "
+  val a = 3
+  val b = 5
+  a ++ b
+"
+
+s %~% "
+  val a = 3
+  val b = 5
+  (a,b)
+"
+
+s %.~% "
+  val a = 3
+  val b = 5
+  (a,b)
+"
+
+tryCatch(s$xaa,error = function(e) {'Caught'})
+s$xaa <- NULL
+tryCatch(s$xaa,error = function(e) {'Caught'})
+
+s$xaa <- 3
+s$xaa
+s$.xaa
+
+withNA <- c(1,2,3,NA,4)
+s$withNA <- withNA
+
+withNaN <- c(1,2,3,NaN,4)
+s$withNaN <- withNaN
+
+identical(  withNA,  withNaN)
+identical(s$withNA,s$withNaN)
+
+withMax <- c(1,.Machine$double.xmax,.Machine$double.xmin)
+s$withMax <- withMax
+identical(withMax,s$withMax)
+
+withMax <- c(1L,.Machine$integer.max)
+s$withMax <- withMax
+identical(withMax,s$withMax)
+
