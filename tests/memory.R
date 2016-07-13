e2849efa6b2ce23a5cd5a0cf9af653a5642a9e96
David B. Dahl <david@ddahl.org>
2016-07-13 09:22:41 -0600

    Fixed typo.

--- tests/memory.R
+++ tests/memory.R
@@ -2,21 +2,23 @@ library(rscala)
 
 serialize <- as.logical(Sys.getenv("RSCALA_SERIALIZE"))
 cat(serialize,"\n")
-s <- scala(serialize=serialize)
 
 s <- scala(heap.max="64M")
 s %~% "util.Properties.versionNumberString"
 a <- tryCatch(s$do("Array[Double]")$new(10000000L),error=function(e) e)
 close(s)
+cat("----\n")
 
 s <- scala(heap.max="256M")
 s %~% "util.Properties.versionNumberString"
 a <- s$do("Array[Double]")$new(10000000L)
 close(s)
+cat("----\n")
 
-options(rscala.heap.max="256M")
+options(rscala.heap.maximum="64M")
 s <- scala()
 s %~% "util.Properties.versionNumberString"
 a <- s$do("Array[Double]")$new(10000000L)
 close(s)
+cat("----\n")
 
