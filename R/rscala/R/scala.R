d24ec7d37beb4467b050e1003214a3732dc8057e
David B. Dahl <david@ddahl.org>
2016-11-14 23:11:37 -0700

    Don't need special support for a milestone release 2.12.0-M5

--- R/rscala/R/scala.R
+++ R/rscala/R/scala.R
@@ -666,8 +666,7 @@ close.ScalaInterpreter <- function(con,...) {
 }
 
 .rscalaJar <- function(version="") {
-  if ( version == "2.12.0-M5" ) major.version <- version
-  else if ( version == "" ) major.version <- ".*"
+  if ( version == "" ) major.version <- ".*"
   else major.version <- substr(version,1,4)
   list.files(system.file("java",package="rscala"),pattern=paste("rscala_",major.version,'-.*\\.jar$',sep=""),full.names=TRUE)
 }
