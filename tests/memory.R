cdc7911b101b4dccc2b8527790ca168cd82983d3
David B. Dahl <david@ddahl.org>
2017-04-14 08:19:21 -0600

    Maybe do is not needed.

--- tests/memory.R
+++ tests/memory.R
@@ -1,26 +1,26 @@
 heap.maximum <- "64M"
 source("common.R",print.eval=TRUE)
 
-tryCatch(a <- s$do("Array[Double]")$new(I(20000000L)),error=function(e) e)
+tryCatch(a <- s$".Array[Double]"$new(I(20000000L)),error=function(e) e)
 close(s)
 cat("----\n")
 
 heap.maximum <- "386M"
 source("common.R",print.eval=TRUE)
-tryCatch(a <- s$do("Array[Double]")$new(I(20000000L)),error=function(e) e)
+tryCatch(a <- s$".Array[Double]"$new(I(20000000L)),error=function(e) e)
 close(s)
 cat("----\n")
 
 options(rscala.heap.maximum="64M")
 source("common.R",print.eval=TRUE)
-tryCatch(a <- s$do("Array[Double]")$new(I(20000000L)),error=function(e) e)
+tryCatch(a <- s$".Array[Double]"$new(I(20000000L)),error=function(e) e)
 close(s)
 cat("----\n")
 
 options(rscala.heap.maximum="128M")
 source("common.R",print.eval=TRUE)
 for ( i in 1:2000 ) {
-  a <- s$do("Array[Double]")$new(I(100000L))  # No memory problems
+  a <- s$".Array[Double]"$new(I(100000L))  # No memory problems
 }
 
 tryCatch({
