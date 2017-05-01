3630d9b877fe8947a3783d3129c3c5ae86512901
David B. Dahl <david@ddahl.org>
2017-05-01 15:48:38 -0600

    Updated all tests.

--- tests/identifiers.R
+++ tests/identifiers.R
@@ -9,14 +9,8 @@ s %~% "3+4"
 
 tryCatch(s$adf,error = function(e) e)
 
-tryCatch(s$.val("def"),error = function(e) e)
 tryCatch(s$def <- 4,error = function(e) e)
 s %~% "3+4"
-
-f <- s$ def(x=I(numeric()),y=I(numeric())) %~% '
-  x+y
-'
-f(3,4)
 
 x <- as.character(0:8)
 set.seed(234235)
