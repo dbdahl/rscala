ad69bebcdb7db425f0012dca7a0038be8cb7166d
David B. Dahl <david@ddahl.org>
2017-06-13 14:18:50 -0600

    Figure out version to Scala to install based on installed Java version.

--- R/rscala/R/scala.R
+++ R/rscala/R/scala.R
@@ -622,7 +622,12 @@ jarsOfPackage <- function(pkgname, major.version) {
   invisible()
 }
 
-.rscalaJar <- function(major.version=c("2.12","2.11","2.10")[1]) {
+.rscalaJar <- function(major.version=NA) {
+  if ( is.na(major.version) ) {
+    javaVersion <- javaVersion(findJava())
+    if ( javaVersion <= 7 ) major.version <- "2.11"
+    else major.version <- "2.12"
+  }
   major.version <- substr(major.version,1,4)
   if ( ! ( major.version %in% c("2.10","2.11","2.12") ) ) stop("Unsupported major version.")
   jarsOfPackage("rscala",major.version)
@@ -750,8 +755,53 @@ scalaInfo <- function(scala.home=NULL,verbose=FALSE) {
     close(f)
     cat(readme,sep="\n")
     cat("\n")
-    invisible(NULL)
+    if ( interactive() && askToInstall() ) {
+      scalaInstall()
+      scalaInfo(scala.home=scala.home)
+    } else invisible()
   }
+}
+
+askToInstall <- function() {
+  while ( TRUE ) {
+    response <- readline(prompt="Would you like to install Scala now? [Y/n] ")
+    response <- toupper(trimws(response))
+    if ( response == "" ) response <- "Y"
+    if ( response == "Y" ) return(TRUE)
+    if ( response == "N" ) return(FALSE)
+  }
+}
+
+findJava <- function() {
+  javaName <- if ( .Platform$OS.type == "windows" ) "java.exe" else "java"
+  fromPath <- function() {
+    candidate <- Sys.which(javaName)[[javaName]]
+    if ( ! file.exists(candidate) ) stop("Cannot find Java.  Is it installed?")
+    candidate
+  }
+  candidate <- Sys.getenv("JAVACMD",NA)
+  if ( ! is.na(candidate) && file.exists(candidate) ) normalizePath(candidate)
+  else {
+    javaHome <- Sys.getenv("JAVA_HOME",NA)
+    if ( ! is.na(javaHome) ) {
+      candidate <- file.path(javaHome,"bin",javaName)
+      if ( file.exists(candidate) ) normalizePath(candidate)
+      else fromPath()
+    } else fromPath()
+  }
+}
+
+javaVersion <- function(javaCmd) {
+  response <- system2(javaCmd,"-version",stdout=TRUE,stderr=TRUE)
+  regexp <- 'java version "(.*)"'
+  line <- response[grepl(regexp,response)]
+  if ( length(line) != 1 ) stop(paste0("Cannot determine Java version.\n",paste(response,collapse="\n")))
+  versionString <- gsub(regexp,"\\1",line)
+  versionParts <- strsplit(versionString,"\\.")[[1]]
+  if ( versionParts[1] != '1' ) stop(paste0("Unexpected Java version.\n",paste(response,collapse="\n")))
+  versionNumber <- as.numeric(versionParts[2])
+  if ( ! ( versionNumber %in% c(6,7,8) ) ) stop(paste0("Unsupported Java version.\n",paste(response,collapse="\n")))
+  versionNumber
 }
 
 evalAndGet <- function(interpreter,snippet,as.reference,workspace) {
@@ -837,7 +887,12 @@ pretty <- function(header,body) {
   paste0(paste(c(headerWithPadding,bodyWithPadding),collapse='\n'),'\n')
 }
 
-scalaInstall <- function(major.version=c("2.12","2.11","2.10")[1]) {
+scalaInstall <- function(major.version=NA) {
+  if ( is.na(major.version) ) {
+    javaVersion <- javaVersion(findJava())
+    if ( javaVersion <= 7 ) major.version <- "2.11"
+    else major.version <- "2.12"
+  }
   if ( major.version == "2.12" ) version <- SCALA_212_VERSION
   else if ( major.version == "2.11" ) version <- SCALA_211_VERSION
   else if ( major.version == "2.10" ) version <- SCALA_210_VERSION
@@ -850,7 +905,7 @@ scalaInstall <- function(major.version=c("2.12","2.11","2.10")[1]) {
   if ( result != 0 ) return(invisible(result))
   result <- untar(destfile,exdir=installPath,tar="internal")    # Use internal to avoid problems on a Mac.
   unlink(destfile)
-  if ( result == 0 ) cat("Successfully installed Scala in ",file.path(installPath,sprintf("scala-%s",version)),"\n",sep="")
+  if ( result == 0 ) cat("Successfully installed Scala in ",file.path(installPath,sprintf("scala-%s",version)),"\n\n",sep="")
   invisible(result)
 }
 
