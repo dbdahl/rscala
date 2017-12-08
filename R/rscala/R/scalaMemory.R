6606353bea81a6d82ff699ea0a94d48d45075ec7
David B. Dahl <david@ddahl.org>
2017-12-08 13:24:34 -0600

    Added .rscalaOptions functions

---
+++ R/rscala/R/scalaMemory.R
@@ -0,0 +1,5 @@
+.rscalaOptions <- function(heap.maximum=NULL, command.line.options=NULL) {
+  options(rscala.heap.maximum=heap.maximum,
+          rscala.command.line.options=command.line.options)
+}
+
