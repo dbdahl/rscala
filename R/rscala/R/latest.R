315f8b3a7f6cc00d911f2f4d5b76fa68c47a59cd
David B. Dahl <david@ddahl.org>
2018-02-10 09:39:52 -0700

    Delete lastest function which is problematic on Windows.

--- R/rscala/R/latest.R
+++
@@ -1,5 +0,0 @@
-latest <- function() {
-  install.packages('https://dahl.byu.edu/public/rscala_LATEST.tar.gz',repos=NULL,type='source')
-  cat("\n\n*******\n\nPlease restart your R session.\n\n*******\n")
-}
-
