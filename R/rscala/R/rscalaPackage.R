c85cef8498fb0c51af889e524ddb229b881cf98b
David B. Dahl <dahl@stat.byu.edu>
2018-06-21 07:49:09 -0600

    Depending packages and installation.

--- R/rscala/R/rscalaPackage.R
+++ R/rscala/R/rscalaPackage.R
@@ -6,9 +6,14 @@
                            mode="",
                            ...) {
   pkgEnv <- parent.env(parent.frame())    # Environment of depending package (assuming this function is only called in .onLoad function).
-  if ( is.null(assign.name) || ( assign.name == "" ) ) stop("'assign.name' must be supplied")
-  if ( mode == "" ) scala(packages,assign.callback,assign.name,pkgEnv,...)
+  if ( is.null(assign.name) || ( assign.name == "" ) ) stop("'assign.name' must be supplied.")
+  if ( mode == "" ) {
+    assign("rscalaBridgeOwner",TRUE,envir=pkgEnv)
+    assign("rscalaBridgeName",assign.name,envir=pkgEnv)
+    scala(packages,assign.callback,assign.name,pkgEnv,...)
+  }
   else {
+    assign("rscalaBridgeOwner",FALSE,envir=pkgEnv)
     delayedAssign(assign.name,{
       s <- eval(parse(text=mode))
       assign.callback(s)
@@ -17,22 +22,9 @@
   }
 }
 
-.rscalaPackageOld <- function(pkgname, ..., classpath.packages=character(), mode="parallel", assign.name="s") {
-  env <- parent.env(parent.frame())    # Environment of depending package (assuming this function is only called in .onLoad function).
-  rscalaPackageEnv <- new.env(parent=emptyenv())
-  assign(".rscalaPackageEnv",rscalaPackageEnv, envir=env)
-  assign("isConnected",FALSE,envir=rscalaPackageEnv)
-  assign("assign.name",assign.name,envir=rscalaPackageEnv)
-  callback <- if ( ! ( mode %in% c("parallel","lazy","serial") ) ) function(ss) {}
-  else function(ss) { assign("isConnected",TRUE,envir=rscalaPackageEnv) }
-  scala(classpath.packages=c(pkgname,classpath.packages),assign.env=env,callback=callback,mode=mode,assign.name=assign.name,...)
-  invisible()
-}
-
+#' @export
+#'
 .rscalaPackageUnload <- function() {
-  env <- parent.env(parent.frame())
-  if ( get("isConnected",envir=get(".rscalaPackageEnv",envir=env)) ) {
-    assign.name <- get("assign.name",envir=get(".rscalaPackageEnv",envir=env))
-    close(get(assign.name,envir=env))
-  }
+  pkgEnv <- parent.env(parent.frame())    # Environment of depending package (assuming this function is only called in .onUnload function).
+  if ( pkgEnv[["rscalaBridgeOwner"]] ) close(pkgEnv[[pkgEnv[["rscalaBridgeName"]]]])
 }
--- R/rscala/R/scalaInstall.R
+++ R/rscala/R/scalaInstall.R
@@ -15,10 +15,10 @@ scalaInstall <- function() {
   if ( result != 0 ) stop(paste0("Please delete the following directory and retry the installation: ",installPath))
   url <- sprintf("https://downloads.lightbend.com/scala/%s/scala-%s.tgz",CANONICAL_SCALA_VERSION,CANONICAL_SCALA_VERSION)
   tgzFile <- tempfile("rscala-tgz-")
-  result <- download.file(url,tgzFile)
+  result <- utils::download.file(url,tgzFile)
   if ( result != 0 ) stop("Unable to download Scala.")
   tmpDir <- paste0(installPath,"-tmp")
-  result <- untar(tgzFile,exdir=tmpDir,tar="internal")    # Use internal to avoid problems on a Mac.
+  result <- utils::untar(tgzFile,exdir=tmpDir,tar="internal")    # Use internal to avoid problems on a Mac.
   unlink(tgzFile)
   if ( result != 0 ) stop("Unsuccessful installation of Scala in ",installPath,"\n\n",sep="")
   file.rename(list.files(tmpDir,full.names=TRUE),installPath)
