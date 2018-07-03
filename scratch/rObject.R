1371ad36b0604ed7b7291d25c15b8fc908982ab8
David B. Dahl <dahl@stat.byu.edu>
2018-07-03 15:38:18 -0600

    Weird bug with R's garbage collector.

--- scratch/rObject.R
+++ scratch/rObject.R
@@ -1,5 +1,4 @@
 library(rscala)
-library(microbenchmark)
 
 scala()
 
@@ -10,7 +9,7 @@ sampler <- function() {
 mm <- function(n) s(sf=s-sampler,n=as.integer(n[1])) * '
   var time = System.nanoTime()
   List.tabulate(n) { i =>
-    if ( i % 100 == 0 ) R.eval("gc()")
+    if ( i % 100 == 0 ) R.eval("gc()")  // The need for this is weird!
     val newTime = System.nanoTime()
     val lapse = newTime - time
     time = newTime
@@ -19,108 +18,7 @@ mm <- function(n) s(sf=s-sampler,n=as.integer(n[1])) * '
   }
 '
 
-s$showCode <- TRUE
-system.time(mm(1))
-system.time(mm(1))
-
-s$debug <- FALSE
 system.time(mm(1000))
-s$debug <- TRUE
 system.time(mm(1000))
-system.time(mm(10000))
-
-
-
-b <- mm(10)
-library(microbenchmark)
-microbenchmark(
-  mm(1)
-  ,
-  mm(10)
-  ,
-  mm(100)
-  ,
-  mm(500)
-  ,
-  mm(1000)
-  ,
-  mm(10000)
-  ,times=10)
-
-b <- mm(1000)
-
--(s - list(1,2,3))
--(s - I(list(1,2,3)))
-
-identical(-b, -(s - (-b)))
-
-
-
-
-microbenchmark(
-  -b
-  ,
-  sapply(-b,function(i) i) 
-  ,times=10)
-
-
-
-undo <- function(b) {
-  nn <- s(mm=b) ^ '
-    (mm.flatMap(_.x), mm.scanLeft(1)((sum,x) => sum + x.x.length))
-  '
-  bytes <- nn$"_1"()
-  sizes <- nn$"_2"()
-  lapply(seq_along(sizes[-1]), function(i) {
-    unserialize(bytes[sizes[i]:(sizes[i+1]-1)])
-  })
-}
-p <- undo(b)
-
-
-
-nn <- s(mm=mm) * '
-  mm.map(_.x)
-'
-unlist(apply(nn,1,function(i) unserialize(i)))
-
-for ( i in seq_along(sizes) ) {
-  unserialize(arr
-}
-
-y <- s$R.evalObject
-microbenchmark(
-lapply(1:10000, function(i) s$R.evalObject("%-()",s-sampler)),
-lapply(1:10000, function(i) s$R.evalObject("%-()",sf)),
-lapply(1:10000, function(i) y("%-()",sf)),
-sapply(e, function(i) -i),
-times=3)
-
-p <- s$R.evalObject("sapply(1:10000, function(i) %-())",sf)
--p
-
-
-microbenchmark(
-  1:100,
-  as.integer(1:100),
-  times=1000)
-
-
-
-y <- s - rnorm
-(-y)(100)
-
-doit <- function(x, nSamples) {
-  s(y=s-x) * 'R.evalD1("I(%-(nSamples))",y)'
-}
-
-microbenchmark(
-  doit(rnorm,1),
-  rnorm(1),
-  doit(rnorm,1000),
-  rnorm(1000),
-  times=100)
-
-
-
+system.time(mm(100000))
 
