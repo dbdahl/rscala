119700b0f810d9f4f310197691261c81b33adc1c
David B. Dahl <dahl@stat.byu.edu>
2018-07-04 06:21:39 -0600

    Improved tests for improved casting in embedded R.

--- R/rscala/tests/testthat/tests-callbacks.R
+++ R/rscala/tests/testthat/tests-callbacks.R
@@ -2,46 +2,57 @@ context("callbacks")
 
 # skip("callbacks")
 
+x <- 1
+
 test_that("primitives work in callbacks", {
-  x <- as.integer(1); expect_identical(s(x=x) * 'R.evalI0("%-",x)', x)
-  x <- as.integer(1); expect_identical(s(x=I(x)) * 'R.evalI0("%-",x)', x)
-  x <- as.integer(1); expect_error(s(x=I(x)) * 'R.evalI0("I(%-)",x)', "^Invocation error")
-  x <- as.double(1); expect_identical(s(x=x) * 'R.evalD0("%-",x)', x)
-  x <- as.double(1); expect_identical(s(x=I(x)) * 'R.evalD0("%-",x)', x)
-  x <- as.double(1); expect_error(s(x=I(x)) * 'R.evalD0("I(%-)",x)', "^Invocation error")
-  x <- as.logical(1); expect_identical(s(x=x) * 'R.evalL0("%-",x)', x)
-  x <- as.logical(1); expect_identical(s(x=I(x)) * 'R.evalL0("%-",x)', x)
-  x <- as.logical(1); expect_error(s(x=I(x)) * 'R.evalL0("I(%-)",x)', "^Invocation error")
-  x <- as.raw(1); expect_identical(s(x=x) * 'R.evalR0("%-",x)', x)
-  x <- as.raw(1); expect_identical(s(x=I(x)) * 'R.evalR0("%-",x)', x)
-  x <- as.raw(1); expect_error(s(x=I(x)) * 'R.evalR0("I(%-)",x)', "^Invocation error")
-  x <- as.character(1); expect_identical(s(x=x) * 'R.evalS0("%-",x)', x)
-  x <- as.character(1); expect_identical(s(x=I(x)) * 'R.evalS0("%-",x)', x)
-  x <- as.character(1); expect_error(s(x=I(x)) * 'R.evalS0("I(%-)",x)', "^Invocation error")
+  expect_identical(s(x=x) * 'R.evalI0("%-",x)', as.integer(x))
+  expect_identical(s(x=I(x)) * 'R.evalI0("%-",x)', as.integer(x))
+  expect_error(s(x=I(x)) * 'R.evalI0("I(%-)",x)', "^Invocation error")
+  expect_identical(s(x=x) * 'R.evalD0("%-",x)', x)
+  expect_identical(s(x=I(x)) * 'R.evalD0("%-",x)', x)
+  expect_error(s(x=I(x)) * 'R.evalD0("I(%-)",x)', "^Invocation error")
+  expect_identical(s(x=x) * 'R.evalL0("%-",x)', as.logical(x))
+  expect_identical(s(x=I(x)) * 'R.evalL0("%-",x)', as.logical(x))
+  expect_error(s(x=I(x)) * 'R.evalL0("I(%-)",x)', "^Invocation error")
+  expect_identical(s(x=x) * 'R.evalR0("%-",x)', as.raw(x))
+  expect_identical(s(x=I(x)) * 'R.evalR0("%-",x)', as.raw(x))
+  expect_error(s(x=I(x)) * 'R.evalR0("I(%-)",x)', "^Invocation error")
+  expect_identical(s(x=x) * 'R.evalS0("%-",x)', as.character(x))
+  expect_identical(s(x=I(x)) * 'R.evalS0("%-",x)', as.character(x))
+  expect_error(s(x=I(x)) * 'R.evalS0("I(%-)",x)', "^Invocation error")
 })
 
 test_that("length 1 vectors work in callbacks", {
-  x <- as.integer(1); expect_identical(s(x=I(x)) * 'R.evalI1("I(%-)",x)', x)
-  x <- as.double(1); expect_identical(s(x=I(x)) * 'R.evalD1("I(%-)",x)', x)
-  x <- as.logical(1); expect_identical(s(x=I(x)) * 'R.evalL1("I(%-)",x)', x)
-  x <- as.raw(1); expect_identical(s(x=I(x)) * 'R.evalR1("I(%-)",x)', x)
-  x <- as.character(1); expect_identical(s(x=I(x)) * 'R.evalS1("I(%-)",x)', x)
+  expect_identical(s(x=I(x)) * 'R.evalI1("I(%-)",x)', as.integer(x))
+  expect_identical(s(x=I(x)) * 'R.evalD1("I(%-)",x)', x)
+  expect_identical(s(x=I(x)) * 'R.evalL1("I(%-)",x)', as.logical(x))
+  expect_identical(s(x=I(x)) * 'R.evalR1("I(%-)",x)', as.raw(x))
+  expect_identical(s(x=I(x)) * 'R.evalS1("I(%-)",x)', as.character(x))
 })
+
+x <- c(1,2)
 
 test_that("length 2 vectors work in callbacks", {
-  x <- as.integer(c(1,2)); expect_identical(s(x=x) * 'R.evalI1("%-",x)', x)
-  x <- as.double(c(1,2)); expect_identical(s(x=x) * 'R.evalD1("%-",x)', x)
-  x <- as.logical(c(1,2)); expect_identical(s(x=x) * 'R.evalL1("%-",x)', x)
-  x <- as.raw(c(1,2)); expect_identical(s(x=x) * 'R.evalR1("%-",x)', x)
-  x <- as.character(c(1,2)); expect_identical(s(x=x) * 'R.evalS1("%-",x)', x)
+  expect_identical(s(x=x) * 'R.evalI1("%-",x)', as.integer(x))
+  expect_identical(s(x=x) * 'R.evalD1("%-",x)', x)
+  expect_identical(s(x=x) * 'R.evalL1("%-",x)', as.logical(x))
+  expect_identical(s(x=x) * 'R.evalR1("%-",x)', as.raw(x))
+  expect_identical(s(x=x) * 'R.evalS1("%-",x)', as.character(x))
 })
 
+x <- matrix(c(0,1),nrow=1)
+xi <- x; storage.mode(xi) <- "integer"
+xd <- x; storage.mode(xd) <- "double"
+xl <- x; storage.mode(xl) <- "logical"
+xr <- x; storage.mode(xr) <- "raw"
+xc <- x; storage.mode(xc) <- "character"
+
 test_that("matrices work in callbacks", {
-  x <- matrix(as.integer(c(1,2)),nrow=1); expect_identical(s(x=x) * 'R.evalI2("%-",x)', x)
-  x <- matrix(as.double(c(1,2)),nrow=1); expect_identical(s(x=x) * 'R.evalD2("%-",x)', x)
-  x <- matrix(as.logical(c(0,1)),nrow=1); expect_identical(s(x=x) * 'R.evalL2("%-",x)', x)
-  x <- matrix(as.raw(c(1,2)),nrow=1); expect_identical(s(x=x) * 'R.evalR2("%-",x)', x)
-  x <- matrix(as.character(c(1,2)),nrow=1); expect_identical(s(x=x) * 'R.evalS2("%-",x)', x)
+  expect_identical(s(x=x) * 'R.evalI2("%-",x)', xi)
+  expect_identical(s(x=x) * 'R.evalD2("%-",x)', xd)
+  expect_identical(s(x=x) * 'R.evalL2("%-",x)', xl)
+  expect_identical(s(x=x) * 'R.evalR2("%-",x)', xr)
+  expect_identical(s(x=x) * 'R.evalS2("%-",x)', xc)
 })
 
 test_that("misc. stuff works as expected", {
