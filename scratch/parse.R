975f90f5cb56f0007ba800721483ff126ab396d7
David B. Dahl <dahl@stat.byu.edu>
2018-07-05 14:54:12 -0600

    Experimenting with functions

--- scratch/parse.R
+++ scratch/parse.R
@@ -1,86 +1,66 @@
-a <- quote({
- m <- 1 + 3
- rnorm(1,mean=mean,sd=2)
-})
+library(rscala)
+scala()
 
-a <- quote( m <- 1 )
 
 r2scala <- function(x) {
-  classes <- sapply(x,function(y) class(y))
-  strings <- sapply(x,function(y) toString(y))
-  if ( length(x) == 1 ) {
-    if ( classes[1] == "name" ) paste0(strings[1])
-    else if ( classes[1] == "numeric" ) paste0("{ ",strings[1],": Double }")
-    else if ( classes[1] == "integer" ) paste0("{ ",strings[1],": Int }")
-    else if ( classes[1] == "character" ) paste0('{ "',strings[1],'": String }')
-    else { cat("2\n"); browser() }
-  }
-  else if ( length(x) == 2 ) {
-    if ( classes[1] == "name" ) {
-      if ( strings[1] == "(" ) paste0("( ",r2scala(x[[2]])," )")
-      else paste0(strings[1],"(",r2scala(x[[2]]),")")
-    }
-  }
-  else if ( length(x) == 3 ) {
-    if ( classes[1] == "name" ) {
-      if ( ( strings[1] == "<-" ) && ( classes[2] == "name" ) ) {
-        paste0("val ", strings[2], " = ", r2scala(x[[3]]))
-      } else if ( strings[1] == "+" ) {
-        paste0(r2scala(x[[2]]), " + ", r2scala(x[[3]]))
-      } else if ( strings[1] == "-" ) {
-        paste0(r2scala(x[[2]]), " - ", r2scala(x[[3]]))       
-      } else if ( strings[1] == "*" ) {
-        paste0(r2scala(x[[2]]), " * ", r2scala(x[[3]]))       
-      } else if ( strings[1] == "/" ) {
-        paste0(r2scala(x[[2]]), " / ", r2scala(x[[3]]))
-      } else paste0(strings[1],"(",r2scala(x[[2]]),",",r2scala(x[[3]]),")")
-    } else { cat("4\n"); browser() }
-  } else { cat("5\n"); browser() }
-}
-
-assert <- function(x) {
-  if ( !x ) stop()
-}
-
-r2scala <- function(x) {
+  if ( length(x) == 0 ) return("")
   typeof  <- sapply(x,function(y) typeof(y))
   classes <- sapply(x,function(y) class(y))
   strings <- sapply(x,function(y) toString(y))
-  cat("--\n")
-  
-  if ( typeof[1] == "symbol" ) paste(strings[1])
-  else if ( classes[1] == "name" ) {
-    if ( strings[1] == "<-" ) {
-      assert(length(x)==3)
-      paste0("val ", r2scala(x[[2]]), " = ", r2scala(x[[3]]))
-    } else if ( strings[1] == "(" ) {
-      assert(length(x)==2)
-      paste0("( ",r2scala(x[[2]])," )")
-    } else if ( strings[1] == "/" ) {
-      assert(length(x)==3)
-      paste0(r2scala(x[[2]]),".toDouble",strings[1],r2scala(x[[3]]))
-    } else if ( strings[1] %in% c("+","-","*") ) {
-      assert(length(x)==3)
-      paste0(r2scala(x[[2]]),strings[1],r2scala(x[[3]]))
-    } else {
-      assert(length(x)==1)
-      strings[1]
-    }
+  cat("<<\n")
+  print(typeof)
+  print(classes)
+  print(strings)
+  cat(">>\n")
+  if ( ( typeof[1] == "symbol" ) && ( classes[1] == "name" ) ) {
+    if ( strings[1] == "{" ) paste0("{\n",paste0(sapply(x[-1],r2scala),collapse="\n"),"\n}")
+    else if ( strings[1] == "<-" ) paste0("val ",r2scala(x[[2]])," = ",r2scala(x[[3]]))
+    else if ( strings[1] == ":" ) paste0("Array.range(",r2scala(x[[2]]),".toInt,",r2scala(x[[3]]),".toInt+1)")
+    else if ( strings[1] == "(" ) paste0("(",r2scala(x[[2]]),")")
+    else if ( strings[1] == "+" ) if ( length(x) == 3 ) paste0(r2scala(x[[2]])," + ",r2scala(x[[3]])) else paste0("+",r2scala(x[[2]]))
+    else if ( strings[1] == "-" ) if ( length(x) == 3 ) paste0(r2scala(x[[2]])," - ",r2scala(x[[3]])) else paste0("-",r2scala(x[[2]]))
+    else if ( strings[1] == "*" ) paste0(r2scala(x[[2]])," * ",r2scala(x[[3]]))
+    else if ( strings[1] == "/" ) paste0(r2scala(x[[2]]),".toDouble / ",r2scala(x[[3]]))
+    else if ( strings[1] == "%%" ) paste0(r2scala(x[[2]])," % ",r2scala(x[[3]]))
+    else if ( strings[1] == "^" ) paste0("pow(",r2scala(x[[2]]),",",r2scala(x[[3]]),")")
+    else if ( ! is.list(x) && is.symbol(x) ) paste0(strings[1])
+    else paste0(strings[1],"(",paste0(sapply(x[-1],r2scala),collapse=","),")")
   }
-  else if ( classes[1] == "numeric" ) paste0("{",strings[1],":Double}")
-  else if ( classes[1] == "integer" ) paste0("{",strings[1],":Int}")
-  else if ( classes[1] == "character" ) paste0('{"',strings[1],'":String}')
-  else { cat("2\n"); browser() }
+  else if ( typeof[1] == "integer" ) paste0("{",strings[1],":Int}")
+  else if ( typeof[1] == "double" ) paste0("{",strings[1],":Double}")
+  else if ( typeof[1] == "character" ) paste0('{"',strings[1],'":String}')
+  else stop("33")
 }
 
-r2scala(quote(m <- 1L))
+s * r2scala(quote(m <- 1:4))
+
+s * r2scala(quote("DavidDahl"))
+
+s * r2scala(quote(1 - 3L))
+
 r2scala(quote(davidDahl <- 1 + 3L))
 r2scala(quote(davidDahl <- 1L / 3L))
-r2scala(quote(davidDahl <- 1 + 3 / ( 2 * 4 )))
+d <- s * paste0("(x: Double) => {\n",r2scala(quote(x + 3 / ( 2 * 4 * 3 ))),"\n}")
 r2scala(quote(rbob(1,3L)))
 
 r2scala(quote(rbob))
 r2scala(quote(rbob()))
+r2scala(quote(rbob(2,mean(1,2,3))))
+r2scala(quote(rbob(2,3,4)))
+
+cat(r2scala(quote({
+  rbob(2,3,4)
+  3+4
+})))
+
+cat(r2scala(quote({
+  x <- rnorm(10)
+  mean(x)
+})))
+
+cat(r2scala(quote({
+  -0.5*log(2*pi*sigma^2) - 0.5/sigma^2 * (x-mean)^2
+})))
 
 a <- quote(rbob)
 b <- quote(rbob())
