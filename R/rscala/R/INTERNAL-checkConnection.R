c71d740b94838b821f677ab050d107d599dfa135
David B. Dahl <dahl@stat.byu.edu>
2018-07-29 01:04:42 -0700

    Better error message.

--- R/rscala/R/INTERNAL-checkConnection.R
+++ R/rscala/R/INTERNAL-checkConnection.R
@@ -1,6 +1,6 @@
 checkConnection <- function(details) {
   if ( Sys.getpid() != details[["pid"]] ) {
     close.rscalaBridge(details)
-    stop("Bridge is closed.")
+    stop("ERROR:  Each process should have its own rscala bridge.  Closing this bridge.")
   }
 }
