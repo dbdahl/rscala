e836c9416405e8ea22af134b82cac9c576200c5b
David B. Dahl <dahl@stat.byu.edu>
2018-11-03 06:39:04 -0600

    More mmap

--- scratch/mmap.scala
+++ scratch/mmap.scala
@@ -1,16 +1,37 @@
+#!/bin/sh
+
+exec scala "$0" "$@"
+
+!#
+
 import java.io.RandomAccessFile
 import java.nio._
 import java.nio.channels._
+import scala.annotation.tailrec
 
-val mem = new RandomAccessFile("/home/dahl/trashme", "rw").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, 100);
+class Prot(val i: Int, val j: Int) {
 
-val zero = 0
-val one  = 1
+  private val mem = new RandomAccessFile("/home/dahl/trashme", "rw").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, 100);
 
-for ( i <- 0 until 100000 ) {
-  while ( mem.getInt(4) == zero ) Thread.sleep(0) // waiting for client request
-  mem.putInt(8, mem.getInt(2) + 1)                // sending the reply
-  mem.putInt(4, zero)
-  mem.putInt(0, one)
+  private val zero = 0
+  private val one  = 1
+
+  if ( i == 0 ) mem.putInt(i,one)
+
+  @tailrec
+  private def talk(counter: Long, nReps: Long): Unit = {
+    while ( mem.getInt(i) == zero ) Thread.sleep(0) // waiting for client request
+    mem.putInt(8, mem.getInt(8) + 1)                // sending the reply
+    mem.putInt(i, zero)
+    mem.putInt(j, one)
+    if ( counter < nReps ) talk(counter+1, nReps) else ()
+  }
+
+  def talk(nReps: Long): Unit = talk(0, nReps)
+
 }
+
+
+val prot = new Prot(args(0).toInt, args(1).toInt)
+prot.talk(100000000L)
 
