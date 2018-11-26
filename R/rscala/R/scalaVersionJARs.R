353496d8953ec1c14c9f965d6bdacea888e65bc0
David B. Dahl <dahl@stat.byu.edu>
2018-11-26 10:19:43 -0700

    Better bootstrapping from a clean repository clone.

--- R/rscala/R/scalaVersionJARs.R
+++ R/rscala/R/scalaVersionJARs.R
@@ -11,6 +11,7 @@
 scalaVersionJARs <- function() {
   pkgHome <- if ( Sys.getenv("RSCALA_BUILDING") != "" ) file.path(getwd(),"inst") else find.package("rscala")
   majorVersions <- gsub("^scala-(.*)","\\1",list.dirs(file.path(pkgHome,"java"),full.names=FALSE,recursive=FALSE))
+  if ( length(majorVersions) == 0 ) return(character(0))
   result <- sapply(majorVersions, function(v) list.files(file.path(pkgHome,"java",paste0("scala-",v)),full.names=TRUE))
   as.list(result[sapply(result, function(x) !is.null(x) && length(x) > 0)])
 }
