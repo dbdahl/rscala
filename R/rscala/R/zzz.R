3b9f527eff60882ba803f819f23a4bf8f0cf5b18
David B. Dahl <dahl@stat.byu.edu>
2019-06-11 20:30:27 -0600

    Better zzz.R

--- R/rscala/R/zzz.R
+++ R/rscala/R/zzz.R
@@ -1,9 +1,5 @@
 if ( identical(Sys.getenv("R_INSTALL_PKG"),"rscala") ) {
-  cat("I'm installing the source package.\n")
-  print(getwd())
   load(file.path("R","sysdata.rda"))
   scalaConfig()
-} else {
-  cat("No, I'm skipping an opportunity.\n")
 }
 
