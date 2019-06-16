e4e6afcce0229fb5a430cd545957af7ae263874d
David B. Dahl <dahl@stat.byu.edu>
2019-06-15 21:20:10 -0600

    Version 3.2.12

--- src/main/scala/org/ddahl/rscala/server/Debugger.scala
+++ src/main/scala/org/ddahl/rscala/server/Debugger.scala
@@ -11,6 +11,7 @@ class Debugger(var on: Boolean, out: java.io.PrintWriter, val label: String, val
       val pretext = "DEBUG (" + label + ") " + (if (withTime) timestamp else "") + ": "
       if (msg.length > maxOutputLength) out.println(pretext + msg.substring(0, maxOutputLength - 3) + "...")
       else out.println(pretext + msg)
+      out.flush()
     }
   }
 
