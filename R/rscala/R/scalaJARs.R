3d2ae8043059777d3d88abcb6633908e9496fdb4
David B. Dahl <dahl@stat.byu.edu>
2019-06-27 11:40:51 -0600

    Improved

--- R/rscala/R/scalaJARs.R
+++ R/rscala/R/scalaJARs.R
@@ -65,7 +65,7 @@ jarsOfPackage <- function(pkgname, major.release) {
   dir <- if ( file.exists(system.file("inst",package=pkgname)) ) file.path("inst/java") else "java"
   jarsMajor <- list.files(file.path(system.file(dir,package=pkgname),paste0("scala-",major.release)),pattern=".*\\.jar$",full.names=TRUE,recursive=FALSE)
   jarsAny <- list.files(system.file(dir,package=pkgname),pattern=".*\\.jar$",full.names=TRUE,recursive=FALSE)
-  result <- c(jarsMajor,jarsAny)
+  result <- normalizePath(c(jarsMajor,jarsAny))
   if ( length(result) == 0 ) {
     supported.versions <- list.files(system.file(dir,package=pkgname),pattern="scala-.*",full.names=FALSE,recursive=FALSE)
     recommended.version <- pickLatestStableScalaVersion(sub("^scala-","",supported.versions))
