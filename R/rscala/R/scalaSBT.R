e274f3e2205a527cd4f34868e603e73807b6d2dc
David B. Dahl <dahl@stat.byu.edu>
2019-07-08 21:18:47 -0600

    Added function scalaDevelDownloadJARs to aid in downloadiing JAR files during package installation.

--- R/rscala/R/scalaSBT.R
+++ R/rscala/R/scalaSBT.R
@@ -119,6 +119,44 @@ scalaDevelDeployJARs <- function(name, root, srcJAR, binJARs) {
   invisible()
 }
 
+#' Download and Deploy JAR Files into the Package File System
+#'
+#' This function should only be called during package installation. That is, it
+#' is meant to be called from bare code of a package that depends on
+#' \pkg{rscala} in a script such as \code{zzz.R}. It downloads JAR files to the
+#' appropriate directories of a package during installation.
+#'
+#' @param description A character vector describing the JAR files to download,
+#'   e,g. \code{"org.apache.commons:commons-math3:3.6.1"}.
+#' @param scalaMajorVersion A Scala major version, such as \code{"2.13"}, if the
+#'   JAR should be placed in a directory specific to a Scala version. Otherwise,
+#'   \code{""} is used to specify a general installation.
+#' @param prefix A string giving the prefix of the download URL.
+#'
+#' @export
+#' @importFrom utils download.file
+#' @examples
+#' \dontrun{
+#' ## To be run in bare code of a package that depends on rscala and needs
+#' ## the Apache Commons Math Library, for example.
+#' rscala::scalaDevelDownloadJARs("org.apache.commons:commons-math3:3.6.1")
+#' }
+#'   
+scalaDevelDownloadJARs <- function(description, scalaMajorVersion="", prefix="https://search.maven.org/remotecontent?filepath=") {
+  if ( identical(Sys.getenv("R_INSTALL_PKG"),"") ) stop("This function should only be called during package installation.")
+  destDir <- file.path(Sys.getenv("R_PACKAGE_DIR"), "java")
+  if ( nchar(scalaMajorVersion) > 0 ) destDir <- file.path(destDir, paste0("scala-", scalaMajorVersion))
+  dir.create(destDir, FALSE, TRUE)
+  for ( w in description ) {
+    cells    <- strsplit(w, ":", fixed=TRUE)[[1]]
+    group    <- gsub("\\.", "/", cells[1])
+    artifact <- cells[2]
+    version  <- cells[3]
+    jar <- sprintf("%s%s/%s/%s/%s.jar",prefix,group,artifact,version,paste(artifact,version,sep="-"))
+    download.file(jar, file.path(destDir, basename(jar)))
+  }
+}
+
 #' @importFrom utils compareVersion
 pickLatestStableScalaVersion <- function(candidates, latestSoFar=NULL) {
   if ( length(candidates) == 0 ) {
