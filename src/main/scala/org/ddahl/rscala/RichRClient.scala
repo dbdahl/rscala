11a41ef0b79abbb97810ebb096b5b7c905faf351
eterk <moshenwuji@live.com>
2023-05-16 17:18:38 +0800

    remove useless code

--- src/main/scala/org/ddahl/rscala/RichRClient.scala
+++ src/main/scala/org/ddahl/rscala/RichRClient.scala
@@ -169,21 +169,6 @@ object RichRClient {
       result
     }
 
-    //
-    //    import scala.language.experimental.macros
-    //    import scala.reflect.macros.blackbox.Context
-    //
-    //    def fieldsNameMacro[T]: Seq[String] = macro fieldsNameMacroImpl[T]
-    //
-    //    def fieldsNameMacroImpl[T: c.WeakTypeTag](c: Context): c.Expr[Seq[String]] = {
-    //      import c.universe._
-    //      val tpe = weakTypeOf[T]
-    //      val fieldNames = tpe.decls.collect {
-    //        case m: MethodSymbol if m.isCaseAccessor => m.name.toString
-    //      }.toSeq.reverse
-    //      c.Expr[Seq[String]](q"$fieldNames")
-    //    }
-
   }
 
 }
